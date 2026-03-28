50f83681d7e8994a4881d4c8b8a05265368fe05b
Syed Nazeer <nazeersd.java@gmail.com>
2026-03-29 00:40:18 +0530

    updated

---
+++ CoreFundas/src/main/java/com/nt/interview/CustomImmutableClass.java
@@ -0,0 +1,82 @@
+package com.nt.interview;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+final public class CustomImmutableClass {
+
+	private final String name;
+	private final int age;
+	private final List<String> hobbies;
+	private final Map<String, Integer> skillset;
+
+	public CustomImmutableClass(String name, int age, List<String> hobbies, Map<String, Integer> skillset) {
+		this.name = name;
+		this.age = age;
+
+		List<String> tempList = new ArrayList<>();
+		for (String hobby : hobbies) {
+			tempList.add(hobby);
+		}
+		this.hobbies = tempList;
+
+		Map<String, Integer> tempMap = new HashMap<String, Integer>();
+		for (Map.Entry<String, Integer> entry : skillset.entrySet()) {
+			tempMap.put(entry.getKey(), entry.getValue());
+		}
+		this.skillset = tempMap;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public List<String> getHobbies() {
+		List<String> tempList = new ArrayList<>();
+		for (String hobby : hobbies) {
+			tempList.add(hobby);
+		}
+		return tempList;
+	}
+
+	public Map<String, Integer> getSkillset() {
+		Map<String, Integer> tempMap = new HashMap<>();
+		for (Map.Entry<String, Integer> entry : skillset.entrySet()) {
+			tempMap.put(entry.getKey(), entry.getValue());
+		}
+		return tempMap;
+	}
+
+	@Override
+	public String toString() {
+		return "Test [ " + name + ", " + age + ", " + hobbies + ", " + skillset + " ]";
+	}
+
+	public static void main(String[] args) {
+
+		List<String> hobbies = new ArrayList<>();
+		hobbies.add("Cricket");
+		hobbies.add("FoosBall");
+		Map<String, Integer> skillset = new HashMap<>();
+		skillset.put("java", 12);
+		skillset.put("SpringBoot", 8);
+		CustomImmutableClass t1 = new CustomImmutableClass("Nazeer", 35, hobbies, skillset);
+		System.out.println(t1);
+
+		System.out.println("Before: " + t1.getHobbies());
+		hobbies.add("Surfing");
+		System.out.println("After: " + t1.getHobbies());
+
+		System.out.println("Before: " + t1.getSkillset());
+		skillset.put("Microservices", 6);
+		System.out.println("After: " + t1.getSkillset());
+
+	}
+
+}
---
+++ CoreFundas/src/main/java/com/nt/interview/FindLongestConsecutiveNumbersCount.java
@@ -0,0 +1,68 @@
+package com.nt.interview;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/*
+ * Find consecutive numbers maximum count
+ * input= {1,2,3,201,202,203,4,300,400};
+ * output=5
+ * 1,2,3,4,5 are the max no of consecutive numbers in array
+ */
+public class FindLongestConsecutiveNumbersCount {
+
+	public static void main(String[] args) {
+		
+		int[] arr = { 1, 2, 3, 201, 202, 203, 4, 300, 5, 400 };
+		// Brute Force approach
+		maxConsecutiveCount(arr);
+		
+		//Optimized Approach
+		findMaxConsecutiveCount(arr);
+
+	}
+
+	private static void findMaxConsecutiveCount(int[] arr) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int n : arr) {
+			// Put all numbers in a HashSet (for O(1) lookup)
+			set.add(n);
+		}
+		int max = 1;
+		for (int num : set) {
+			// Only start counting when the number is beginning of sequence
+			if (!set.contains(num - 1)) {
+				int current = num;
+				int count = 1;
+				// Check next consecutive number is available if yes, increase the count and counter too 
+				//to check the next consecutive numbers availability
+				while (set.contains(current + 1)) {
+					count++;
+					current++;
+				}
+				// Track maximum length
+				max = Math.max(max, count);
+			}
+		}
+		System.out.println(max);
+	}
+
+	private static void maxConsecutiveCount(int[] arr) {
+		int max = 1;
+		int counter = 1;
+		Arrays.sort(arr);
+		for (int i = 1; i < arr.length; i++) {
+			if (Math.abs(arr[i - 1] - arr[i]) == 1)
+				counter++;
+			else {
+				counter = 1;
+			}
+			max = Math.max(max, counter);
+		}
+		System.out.println(max);
+	}
+
+}
