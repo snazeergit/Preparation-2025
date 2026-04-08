e07ba093311dab55c18b09d70bd830f0971141fa
Syed Nazeer <nazeersd.java@gmail.com>
2026-04-09 00:39:00 +0530

    updated

--- CoreFundas/src/main/java/com/nt/streams/StreamsPrep.java
+++ CoreFundas/src/main/java/com/nt/streams/StreamsPrep.java
@@ -16,26 +16,13 @@ public class StreamsPrep {
 
 		List<Employee> empList = Employee.getEmpList();
 
-		/* 
-		 * Dept wise emps whose sal >1000 
-		 */
-		Map<String, List<Employee>> map = empList.stream()
-				.collect(Collectors.groupingBy(Employee::getDept_Name, Collectors.collectingAndThen(Collectors.toList(),
-						list -> list.stream().filter(e -> e.getSalary() > 1000).toList())));
-		// System.out.println(map);
+		Map<String, List<Employee>> map = empList.stream().filter(e -> e.getSalary() > 1000)
+				.collect(Collectors.groupingBy(Employee::getDept_Name)).entrySet().stream()
+				.filter(e -> e.getValue().size() > 1).sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+				.limit(2).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldV, newV) -> oldV,
+						LinkedHashMap::new));
 
-		/*
-		 * Filtering depts who are having more than 1 emp
-		 * Sorting based on dept name
-		 * collect top 2 dept
-		 */
-		LinkedHashMap<String, List<Employee>> map2 = map.entrySet().stream().filter(e -> e.getValue().size() > 1)
-				.sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())).limit(2).collect(Collectors
-						.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldV, newV) -> oldV, LinkedHashMap::new));
-
-		// System.out.println(map2);
-
-		map2.forEach((k, v) -> {
+		map.forEach((k, v) -> {
 			System.out.println(k + " :");
 			for (Employee emp : v) {
 				System.out.println("\t" + emp.getEmp_Name());
