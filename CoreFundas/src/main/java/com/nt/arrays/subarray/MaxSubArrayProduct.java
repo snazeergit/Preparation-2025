ba99f5f15c13fa78a531c67bb597c277a0790f5c
Syed Nazeer <nazeersd.java@gmail.com>
2026-04-13 11:19:48 +0530

    updated

--- CoreFundas/src/main/java/com/nt/arrays/subarray/MaxSubArrayProduct.java
+++ CoreFundas/src/main/java/com/nt/arrays/subarray/MaxSubArrayProduct.java
@@ -7,24 +7,44 @@ public class MaxSubArrayProduct {
 
 		int[] arr = { 2, 3, -4, 3, 2 };
 
+		int maxProd = findMaxProd(arr);
+		System.out.println(maxProd);
+
+		int maxSum = findMaxSum(arr);
+		System.out.println(maxSum);
+
+	}
+
+	private static int findMaxSum(int[] arr) {
+		// TODO Auto-generated method stub
+		int maxSum = arr[0];
+		int currentSum = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			currentSum = Math.max(arr[i], currentSum + arr[i]);
+			maxSum = Math.max(maxSum, currentSum);
+		}
+
+		return maxSum;
+	}
+
+	private static int findMaxProd(int[] arr) {
 		int max = arr[0];
 		int min = arr[0];
 		int target = arr[0];
 
 		for (int i = 1; i < arr.length; i++) {
-			int current = arr[i];
 
-			if (current < 0) {
+			if (arr[i] < 0) {
 				int temp = max;
 				max = min;
 				min = temp;
 			}
-			max = Math.max(current * max, current);
-			min = Math.min(current * min, current);
+			max = Math.max(arr[i] * max, arr[i]);
+			min = Math.min(arr[i] * min, arr[i]);
 			target = Math.max(max, target);
 		}
-
-		System.out.println(target);
+		return target;
 
 	}
 
