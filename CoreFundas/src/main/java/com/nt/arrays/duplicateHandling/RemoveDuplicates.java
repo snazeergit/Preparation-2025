3c7a7633fc5b4770cb41c9820068160ed7191298
Syed Nazeer <nazeersd.java@gmail.com>
2026-04-20 22:40:11 +0530

    updated

--- CoreFundas/src/main/java/com/nt/arrays/duplicateHandling/RemoveDuplicates.java
+++ CoreFundas/src/main/java/com/nt/arrays/duplicateHandling/RemoveDuplicates.java
@@ -2,13 +2,14 @@ package com.nt.arrays.duplicateHandling;
 
 import java.util.Arrays;
 
-public class RemoveDuplicateUnSortedArray {
+public class RemoveDuplicates {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = { 2, 1, 1, 2, 3, 5, 4, 4 };
+		int[] arr = { 1, 1, 2, 3, 4, 4, 5 };
 		int[] unique = Arrays.stream(arr).distinct().toArray();
 		System.out.println(Arrays.toString(unique));
+
 	}
 
 }
